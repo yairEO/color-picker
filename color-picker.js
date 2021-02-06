93b7ff8504e206a701fe83d0bf79df5d49c4f43c
vsync <vsync.design@gmail.com>
2021-02-06 22:00:28 +0200

    0.10.1

--- color-picker.js
+++ color-picker.js
@@ -1,3 +1,10 @@
+/**
+  * @yaireo/color-picker - Lightweight javascript color picker
+  *
+  * @version v0.10.1
+  * @homepage https://yaireo.github.io/color-picker
+  */
+
 
 (function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 (function (global, factory) {
