50acba2940f850434fd63230963d675879c975f6
Yair Even Or <Yair.Even-Or@amdocs.com>
2023-03-26 09:50:16 +0300

    improved code readibility

--- src/history.js
+++ src/history.js
@@ -1,19 +1,26 @@
+/**
+ * Changes history manager
+ */
 export default function history(){
   const historyChange = () => this.settings.onChange(this.CSSColor)
   const setColor = this.setColor.bind(this)
 
   return {
     _value: [this.color],
+
     get pop(){
       return this._value.pop()
     },
+
     get previous(){
       return this._value[this._value.length - 2]
     },
+
     set last( item ){
       this._value.push(item)
       historyChange()
     },
+
     undo(){
       // leave one last item in the value array
       if( this._value.length > 1 ){
