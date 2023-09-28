2bee4a437e34113bf60ebab5b272a56acf632acd
Yair Even Or <vsync.design@gmail.com>
2023-09-28 10:17:03 +0200

    replace deprecated `rollup-plugin-terser` with `@rollup/plugin-terser`

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,5 +1,5 @@
 import babel           from '@rollup/plugin-babel'
-import { terser }      from 'rollup-plugin-terser'
+import terser          from '@rollup/plugin-terser';
 import cleanup         from 'rollup-plugin-cleanup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs        from '@rollup/plugin-commonjs'
