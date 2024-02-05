a72724155fb4deade648d1d2600828feac0644e1
Yair Even Or <vsync.design@gmail.com>
2024-02-05 19:42:28 +0100

    0.13.0

--- color-picker.js
+++ color-picker.js
@@ -1,6 +1,13 @@
 /**
   * @yaireo/color-picker - Lightweight javascript color picker
   *
+  * @version v0.13.0
+  * @homepage https://yaireo.github.io/color-picker
+  */
+
+/**
+  * @yaireo/color-picker - Lightweight javascript color picker
+  *
   * @version v0.12.1
   * @homepage https://yaireo.github.io/color-picker
   */
